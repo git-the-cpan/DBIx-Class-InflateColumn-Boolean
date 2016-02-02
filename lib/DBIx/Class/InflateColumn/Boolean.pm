0154bf737a4886e5e4ece2e2507ee424c5b30c1b
Bernhard Graf <graf@cpan.org>
2016-02-02 23:08:26 +0000

    Import of GRAF/DBIx-Class-InflateColumn-Boolean-0.003000.tar.gz from CPAN.
    gitpan-cpan-distribution: DBIx-Class-InflateColumn-Boolean
    gitpan-cpan-version:      0.003000
    gitpan-cpan-path:         GRAF/DBIx-Class-InflateColumn-Boolean-0.003000.tar.gz
    gitpan-cpan-author:       GRAF
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/InflateColumn/Boolean.pm
+++ lib/DBIx/Class/InflateColumn/Boolean.pm
@@ -5,53 +5,51 @@ use strict;
 
 use base qw/DBIx::Class/;
 
-use Contextual::Return;
-
 =head1 NAME
 
 DBIx::Class::InflateColumn::Boolean - Auto-create boolean objects from columns.
 
 =head1 VERSION
 
-Version 0.001001
+Version 0.003000
 
 =cut
 
-our $VERSION = '0.002000';
+our $VERSION = '0.003000';
 
 =head1 SYNOPSIS
 
 Load this component and declare columns as boolean values.
 
-    package Table;
-    __PACKAGE__->load_components(qw/InflateColumn::Boolean Core/);
-    __PACKAGE__->table('table');
-    __PACKAGE__->true_is('Y');
-    __PACKAGE__->add_columns(
-        foo => {
-            data_type => 'varchar',
-            is_boolean  => 1,
-        },
-        bar => {
-            data_type => 'varchar',
-            is_boolean  => 1,
-            true_is     => qr/^(?:yes|ja|oui|si)$/i,
-        },
-        baz => {
-            data_type => 'int',
-            is_boolean  => 1,
-            false_is    => ['0', '-1'],
-        },
-    );
+  package Table;
+  __PACKAGE__->load_components(qw/InflateColumn::Boolean Core/);
+  __PACKAGE__->table('table');
+  __PACKAGE__->true_is('Y');
+  __PACKAGE__->add_columns(
+      foo => {
+          data_type => 'varchar',
+          is_boolean  => 1,
+      },
+      bar => {
+          data_type => 'varchar',
+          is_boolean  => 1,
+          true_is     => qr/^(?:yes|ja|oui|si)$/i,
+      },
+      baz => {
+          data_type => 'int',
+          is_boolean  => 1,
+          false_is    => ['0', '-1'],
+      },
+  );
 
 Then you can treat the specified column as a boolean:
 
-    print 'table.foo is ', $table->foo ? 'true' : 'false', "\n";
-    print 'table.bar is ', $table->bar ? 'true' : 'false', "\n";
+  print 'table.foo is ', $table->foo ? 'true' : 'false', "\n";
+  print 'table.bar is ', $table->bar ? 'true' : 'false', "\n";
 
 The boolean object still stringifies to the actual field value:
 
-    print $table->foo;  # prints "Y" if it is true
+  print $table->foo;  # prints "Y" if it is true
 
 =head1 DESCRIPTION
 
@@ -65,37 +63,32 @@ But for a database it is up to the designer to decide what is C<true>
 and what is C<false>.
 
 This module maps such "database booleans" into "Perl booleans" and back
-by inflating designated columns into some sort of boolean objects, that
-happen to be instances of L<Contextual::Return::Value|Contextual::Return>.
-Objects of this class can store the original value and a boolean meaning
-at the same time. Therefore - if C<Yes> in the database means C<true>
-and C<No> means C<false> in the application the following two lines can
-virtually mean the same:
+by inflating designated columns into objects that store the original value,
+but also evaluate as true or false in boolean context.  Therefore - if
+"Yes" in the database means C<true> and "No" means C<false> in the
+application the following two lines can virtually mean the same:
 
   if ($table->field eq "No") { ... }
   if (not $table->field) { ... }
 
-That means that C<< $table->field >> has the scalar value C<"No">, but
-is taken as C<false> in a boolean context, whereas without the little
-magic from L<Contextual::Return|Contextual::Return> Perl would regard
-the string C<"No"> as C<true>.
+That means that C<< $table->field >> has the scalar value "No", but
+is taken as C<false> in a boolean context, whereas Perl would normally
+regard the string "No" as C<true>.
 
 When writing to the database, of course C<< $table->field >> would be
-deflated to the original value C<"No"> and not some Perlish form of a
+deflated to the original value "No" and not some Perlish form of a
 boolean.
 
 =head2 Important Notice
 
-It is strongly discouraged to assign a
-L<Contextual::Return|Contextual::Return> object to a boolean field when
-creating a fresh row, because:
+It is strongly encouraged to assign normal database values to a boolean
+field when creating a fresh row, because:
 
 =over 4
 
 =item KISS (http://en.wikipedia.org/wiki/KISS_principle)
 
-Just say "No" when you mean it. It does not buy you anything to say
-C<SCALAR {"No"} BOOL { 0 }>.
+Just say "No" when you mean it.
 
 =item Don't rely on the current boolean class
 
@@ -151,73 +144,91 @@ users.
 
 sub register_column {
     my ($self, $column, $info, @rest) = @_;
+
     $self->next::method($column, $info, @rest);
 
     return unless defined $info->{'is_boolean'};
 
     my ($true_is, $false_is);
+
     defined($true_is = $info->{true_is})
-	or defined($false_is = $info->{false_is})
-	or defined($true_is = $self->true_is)
-	or defined($false_is = $self->false_is)
-	or $true_is = qr/^(y|yes|true|1)$/i;
+        or defined($false_is = $info->{false_is})
+        or defined($true_is = $self->true_is)
+        or defined($false_is = $self->false_is)
+        or $true_is = qr/^(y|yes|true|1)$/i;
 
     my $ref;
     if (defined $false_is) {	# column is false-specific
-	$ref = ref $false_is;
-	$self->inflate_column(
-	    $column => {
-		inflate =>
-		    $ref eq '' ?
-			sub {
-			    my $x = shift;
-			    BLESSED { 'Contextual::Return::Value' } SCALAR { $x } BOOL { $x eq $false_is ? 0 : 1 };
-			} :
-			$ref eq 'ARRAY' ?
-			    sub {
-				my $x = shift;
-				for (@$false_is) {
-				    return BLESSED { 'Contextual::Return::Value' } SCALAR { $x } BOOL { 0 }
-					if $x eq $_;
-				}
-				BLESSED { 'Contextual::Return::Value' } SCALAR { $x } BOOL { 1 };
-			    } :
-			    # $ref eq 'Regexp'
-			    sub {
-				my $x = shift;
-				BLESSED { 'Contextual::Return::Value' } SCALAR { $x } BOOL { $x =~ $false_is ? 0 : 1 };
-			    },
-		deflate => sub { shift },
-	    }
-	);
+        $ref = ref $false_is;
+        $self->inflate_column(
+            $column => {
+                inflate =>
+                    $ref eq '' ?
+                        sub {
+                            my $x = shift;
+                            DBIx::Class::InflateColumn::Boolean::Value->new($x, $x ne $false_is);
+                        } :
+                    $ref eq 'ARRAY' ?
+                        sub {
+                            my $x = shift;
+                            for (@$false_is) {
+                                return DBIx::Class::InflateColumn::Boolean::Value->new($x, 0)
+                                if $x eq $_;
+                            }
+                            DBIx::Class::InflateColumn::Boolean::Value->new($x, 1)
+                        } :
+                        # $ref eq 'Regexp'
+                        sub {
+                            my $x = shift;
+                            DBIx::Class::InflateColumn::Boolean::Value->new($x, $x !~ $false_is);
+                        },
+                deflate => sub { shift },
+            }
+        );
     }
     else {			# column is true-specific
-	$ref = ref $true_is;
-	$self->inflate_column(
-	    $column => {
-		inflate =>
-		    $ref eq '' ?
-			sub {
-			    my $x = shift;
-			    BLESSED { 'Contextual::Return::Value' } SCALAR { $x } BOOL { $x eq $true_is ? 1 : 0 };
-			} :
-			$ref eq 'ARRAY' ?
-			    sub {
-				my $x = shift;
-				for (@$true_is) {
-				    return BLESSED { 'Contextual::Return::Value' } SCALAR { $x } BOOL { 1 }
-					if $x eq $_;
-				}
-				BLESSED { 'Contextual::Return::Value' } SCALAR { $x } BOOL { 0 };
-			    } :
-			    # $ref eq 'Regexp'
-			    sub {
-				my $x = shift;
-				BLESSED { 'Contextual::Return::Value' } SCALAR { $x } BOOL { $x =~ $true_is ? 1 : 0 };
-			    },
-		deflate => sub { shift },
-	    }
-	);
+        $ref = ref $true_is;
+        $self->inflate_column(
+            $column => {
+                inflate =>
+                    $ref eq '' ?
+                        sub {
+                            my $x = shift;
+                            DBIx::Class::InflateColumn::Boolean::Value->new($x, $x eq $true_is);
+                        } :
+                    $ref eq 'ARRAY' ?
+                        sub {
+                            my $x = shift;
+                            for (@$true_is) {
+                                return DBIx::Class::InflateColumn::Boolean::Value->new($x, 1)
+                                if $x eq $_;
+                            }
+                            DBIx::Class::InflateColumn::Boolean::Value->new($x, 0)
+                        } :
+                        # $ref eq 'Regexp'
+                        sub {
+                            my $x = shift;
+                            DBIx::Class::InflateColumn::Boolean::Value->new($x, $x =~ $true_is)
+                        },
+                deflate => sub { shift },
+            }
+        );
+    }
+}
+
+{
+    package #hide
+        DBIx::Class::InflateColumn::Boolean::Value;
+
+    use overload
+        '""' => sub { $_[0][0] },
+        'bool' => sub { $_[0][1] },
+        fallback => 1,
+    ;
+
+    sub new {
+        my ($class, $value, $bool) = @_;
+        my $self = bless [$value, !!$bool], $class;
     }
 }
 
@@ -227,23 +238,23 @@ __END__
 
 =head1 SEE ALSO
 
-L<Contextual::Return>,
 L<DBIx::Class>,
 L<DBIx::Class::InflateColumn>
 
 =head1 AUTHOR
 
-Bernhard Graf C<< <graf at cpan.org> >>
+Bernhard Graf
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-dbix-class-inflatecolumn-bool at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-Class-InflateColumn-Boolean>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests through the web interface at
+L<https://github.com/augensalat/DBIx-Class-InflateColumn-Boolean/issues>.
+I will be notified, and then you'll automatically be notified of
+progress on your bug as I make changes.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Bernhard Graf, all rights reserved.
+Copyright 2008-2016 Bernhard Graf, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
